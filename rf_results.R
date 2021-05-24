f99363c664a10007be28b933e5f325a9df7a0b65
lilyyan2023 <lilyyan2023@u.northwestern.edu>
2021-05-24 13:05:31 -0500

    try regression

--- rf_results.R
+++ rf_results.R
@@ -12,27 +12,28 @@ loan_test <- read_csv("test.csv")
 loan_folds <- vfold_cv(data = loan_train, v = 5, repeats = 3)
 
 # Build recipe
-# loan_recipe <- 
-#   recipe(money_made_inv ~ loan_amnt + out_prncp_inv + application_type +
-#            home_ownership + initial_list_status + term + grade + verification_status, 
-#          data = loan_train) %>% 
-#   step_interact(loan_amnt ~ out_prncp_inv) %>% 
-#   step_dummy(all_nominal_predictors(), one_hot = TRUE) %>% 
-#   step_zv(all_predictors()) %>% 
-#   step_normalize(all_predictors())
-loan_train_try <-
-  loan_train %>% 
-  select(-c(earliest_cr_line, emp_title, id))
-
-loan_recipe2 <- 
-  recipe(money_made_inv ~ ., data = loan_train_try) %>% 
-  step_zv(all_predictors()) %>% 
-  step_other(addr_state, emp_length, last_credit_pull_d, purpose, sub_grade, 
-             threshold = 0.1, other = "other values") %>% 
-  step_dummy(all_nominal_predictors(), one_hot = TRUE) %>% 
+loan_recipe <-
+  recipe(money_made_inv ~ loan_amnt + out_prncp_inv + application_type +
+           initial_list_status + term + grade + verification_status,
+         data = loan_train) %>%
+  step_interact(loan_amnt ~ out_prncp_inv) %>%
+  step_dummy(all_nominal_predictors(), one_hot = TRUE) %>%
+  step_zv(all_predictors()) %>%
   step_normalize(all_predictors())
 
-tibble <- prep(loan_recipe2) %>% 
+# loan_train_try <-
+#   loan_train %>% 
+#   select(-c(earliest_cr_line, emp_title, id))
+# 
+# loan_recipe2 <- 
+#   recipe(money_made_inv ~ ., data = loan_train_try) %>% 
+#   step_zv(all_predictors()) %>% 
+#   step_other(addr_state, emp_length, last_credit_pull_d, purpose, sub_grade, 
+#              threshold = 0.1, other = "other values") %>% 
+#   step_dummy(all_nominal_predictors(), one_hot = TRUE) %>% 
+#   step_normalize(all_predictors())
+
+tibble <- prep(loan_recipe) %>% 
   bake(new_data = NULL)
 
 # random forest model
@@ -45,7 +46,7 @@ rf_model <- rand_forest(
 
 # set-up tuning grid ----
 rf_params <- parameters(rf_model) %>% 
-  update(mtry = mtry(range = c(2,6)))
+  update(mtry = mtry(range = c(2,20)))
 
 # define tuning grid
 rf_grid <- grid_regular(rf_params, levels = c(3,4))
@@ -53,7 +54,7 @@ rf_grid <- grid_regular(rf_params, levels = c(3,4))
 # workflow ----
 rf_workflow <- workflow() %>% 
   add_model(rf_model) %>% 
-  add_recipe(loan_recipe2)
+  add_recipe(loan_recipe)
 
 rf_tuned <- rf_workflow %>% 
   tune_grid(loan_folds, rf_grid)
